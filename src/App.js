2d1fa0ca3b3aabf9a2c92ccd5a39dcca89fd2444
fai-sas <craftedbyfaisal@gmail.com>
2021-12-16 18:42:08 +0600

    files updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
-
 import Photo from './Photo'
+
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
 const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
 const mainUrl = `https://api.unsplash.com/photos/`
+
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
-  const [page, setPage] = useState(0)
+  const [page, setPage] = useState(1)
   const [query, setQuery] = useState('')
+
   const fetchImages = async () => {
     setLoading(true)
     let url
     const urlPage = `&page=${page}`
+
     const urlQuery = `&query=${query}`
+
     if (query) {
       url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
     } else {
       url = `${mainUrl}${clientID}${urlPage}`
     }
+
     try {
       const response = await fetch(url)
       const data = await response.json()
+      // console.log(data)
+
       setPhotos((oldPhotos) => {
         if (query && page === 1) {
           return data.results
@@ -36,19 +46,23 @@ function App() {
       setLoading(false)
     } catch (error) {
       console.log(error)
-      setLoading(false)
+      setLoading(true)
     }
   }
+
   useEffect(() => {
     fetchImages()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line
   }, [page])
 
   useEffect(() => {
     const event = window.addEventListener('scroll', () => {
+      // console.log(`innerHeight ${window.innerHeight}`)
+      // console.log(`scrollY ${window.scrollY}`)
+      // console.log(`bodyHeight ${document.body.scrollHeight}`)
       if (
-        (!loading && window.innerHeight + window.scrollY) >=
-        document.body.scrollHeight - 2
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
       ) {
         setPage((oldPage) => {
           return oldPage + 1
@@ -56,12 +70,16 @@ function App() {
       }
     })
     return () => window.removeEventListener('scroll', event)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line
   }, [])
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
     setPage(1)
+    fetchImages()
   }
+
   return (
     <main>
       <section className='search'>
@@ -69,9 +87,11 @@ function App() {
           <input
             type='text'
             placeholder='search'
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
             className='form-input'
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value)
+            }}
           />
           <button type='submit' className='submit-btn' onClick={handleSubmit}>
             <FaSearch />
@@ -81,10 +101,10 @@ function App() {
       <section className='photos'>
         <div className='photos-center'>
           {photos.map((image, index) => {
-            return <Photo key={index} {...image} />
+            return <Photo key={image.id} {...image} />
           })}
         </div>
-        {loading && <h2 className='loading'>Loading...</h2>}
+        {loading && <h2>loading...</h2>}
       </section>
     </main>
   )
--- src/Photo.js
+++ src/Photo.js
@@ -14,12 +14,12 @@ const Photo = ({
     <article className='photo'>
       <img src={regular} alt={alt_description} />
       <div className='photo-info'>
-        <div>
+        <div className=''>
           <h4>{name}</h4>
           <p>{likes} likes</p>
         </div>
         <a href={portfolio_url}>
-          <img src={medium} alt='' className='user-img' />
+          <img src={medium} alt={name} className='user-img' />
         </a>
       </div>
     </article>
